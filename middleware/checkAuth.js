26aee49d28dd6587d903a2891d0402f9caf149ce
Meguenni Walid <74263244+walidmeguenni@users.noreply.github.com>
2023-07-27 14:41:51 +0100

    Update checkAuth.js

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1]; //""
+    const token = req.headers.authorization.split(" ")[1]; // Breaer" "token
     const decoded = jwt.verify(token, process.env.SECRET_TOKEN);
     req.userData = decoded;
     console.log(req.userData);
